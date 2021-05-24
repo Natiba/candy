e13c3c2235bf9562cff2d8a4dec0364cfd35b942
Nati Ba <nzbartolome@gmail.com>
2021-05-24 15:51:47 -0300

    search allmost ready

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,6 +5,67 @@ function mostrar_menu() {
 }
 
 
+
+
+// Buscador de Contenido
+
+// Ejecutando funciones:
+
+document.getElementById('icon-search').addEventListener('click', show_search); 
+document.getElementById('back-ctn-search').addEventListener('click', hide_search);
+
+// Declaro mis variables:
+
+let ctnBarsSearch = document.getElementById('ctn-bars-search');
+let backCtnSearch = document.getElementById('back-ctn-search');
+ let inputSearch = document.getElementById('inputSearch');
+ console.log(inputSearch);
+let boxSearch = document.getElementById('box-search');
+
+// Funcion para mostrar el buscador:
+
+function show_search() {
+
+ctnBarsSearch.style.top = '140px'; //para que baje xq lo tengo escondido arriba 
+backCtnSearch.style.display = 'block'; //para que aparezca el fondo translucido negro
+inputSearch.focus(); //para que funcione la barra al tipear
+
+}
+
+// Funcion para ocultar el buscador:
+
+function hide_search() {
+
+    ctnBarsSearch.style.top = '-10px'; //para que suba y lo esconda nuevamente arriba 
+    backCtnSearch.style.display = 'none'; //para que tambien se oculte el fondo translucido
+    inputSearch.value = '';  //esto es para que el valor del input quede vacio al volver a abrir el buscador 
+}
+
+// Creando filtrado de busqueda:
+
+document.getElementById('inputSearch').addEventListener('keyup', internal_search); 
+
+function internal_search() {
+
+inputSearch.value = toUpperCase();
+li = boxSearch.getElementsByTagName('li');
+
+//Recorriendo elementos a filtrar mediante los li
+
+for(i = 0; i < li.lenght; i++) {
+
+    a = li[i].getElementsByTagName('a')[0];
+    textValue = a.textContent || a.innerText;
+     
+    if(textValue.toUpperCase().indexOf(filter) > -1) {
+
+    li[i].display ='';
+
+    }
+}
+
+} 
+
 // Esto era para lupa:
 
 // No necesitas Javascript para que esto funcione. Pero creo que una cosa que sí te vendría bien es que, al pulsar sobre el <div>, lleves el foco de la aplicación dentro del campo input.
